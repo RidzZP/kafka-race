fab8f11b56466576ca731c97d4117795e3921e78
namielogs <ridzprog@gmail.com>
2024-02-21 11:26:47 +0700

    new template

---
+++ src/controllers/statusController.js
@@ -0,0 +1,85 @@
+// messageController.js
+import { sendToKafka } from "../services/kafkaService.js";
+import { saveDataToMySQL } from "../models/databaseModel.js";
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "assets/images/race");
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext);
+  },
+});
+
+const upload = multer({ storage: storage }).single("foto");
+
+export async function sendStatus(req, res) {
+  try {
+    upload(req, res, async function (err) {
+      if (err instanceof multer.MulterError) {
+        return res.status(400).json({ error: err.message });
+      } else if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+
+      const {
+        id_kendaraan,
+        no_polisi,
+        id_pengemudi,
+        nama_driver,
+        id_msm,
+        kondisi_kendaraan,
+        action,
+        empty_load,
+        ketrangan,
+        customer,
+        posisi,
+        tujuan,
+        id_user,
+      } = req.body;
+      const foto = req.file.filename;
+
+      await sendToKafka(
+        id_kendaraan,
+        no_polisi,
+        id_pengemudi,
+        nama_driver,
+        id_msm,
+        kondisi_kendaraan,
+        action,
+        empty_load,
+        ketrangan,
+        customer,
+        posisi,
+        tujuan,
+        foto,
+        id_user,
+        foto
+      );
+      await saveDataToMySQL(
+        id_kendaraan,
+        no_polisi,
+        id_pengemudi,
+        nama_driver,
+        id_msm,
+        kondisi_kendaraan,
+        action,
+        empty_load,
+        ketrangan,
+        customer,
+        posisi,
+        tujuan,
+        foto,
+        id_user,
+        foto
+      );
+
+      res.status(200).json({ message: "Message sent successfully" });
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -1,9 +1,11 @@
 // messageRoutes.js
 import express from "express";
 import { sendMessage } from "../controllers/messageController.js";
+import { sendStatus } from "../controllers/statusController.js";
 
 const router = express.Router();
 
 router.post("/", sendMessage);
+router.post("/status", sendStatus);
 
 export { router };
